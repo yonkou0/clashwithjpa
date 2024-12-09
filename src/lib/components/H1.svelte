af8c98e64829d01afb6a0fc3906c858b1097425c
swayam25 <178068815+swayam25@users.noreply.github.com>
2024-12-09 08:12:14 +0530

    Add `H1` component

---
+++ src/lib/components/H1.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    interface Props {
+        class?: string;
+        children: import("svelte").Snippet;
+    }
+    let { class: className = "", children }: Props = $props();
+</script>
+
+<h1 class="font-extrabold shadow-gray-950 drop-shadow-[0_8px_0_var(--tw-shadow-color)] {className}" style="-webkit-text-stroke: 2px var(--tw-shadow-color);">
+    {@render children?.()}
+</h1>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
     import MaterialSymbolsVolumeUpRounded from "~icons/material-symbols/volume-up-rounded";
     import MaterialSymbolsVolumeOffRounded from "~icons/material-symbols/volume-off-rounded";
     import Button from "$lib/components/Button.svelte";
+    import H1 from "$lib/components/H1.svelte";
     import { onMount } from "svelte";
 
     let isPaused: boolean = $state(false);
@@ -47,12 +48,7 @@
 
     <div class="z-10 flex size-full items-center justify-center bg-gray-950/50 p-5 pt-20">
         <div class="flex flex-col items-center text-center sm:w-3/4 md:w-1/2">
-            <h1
-                class="text-4xl font-extrabold shadow-gray-950 drop-shadow-[0_8px_0_var(--tw-shadow-color)] lg:text-5xl"
-                style="-webkit-text-stroke: 2px var(--tw-shadow-color);"
-            >
-                CLASH WITH JPA
-            </h1>
+            <H1 class="text-4xl lg:text-5xl">Clash With JPA</H1>
             <p class="text-md mt-4 lg:text-lg">
                 FWA experts in War-Farming, offering diverse clans and simultaneous 50v50 FWA wars and CWL action. Join one of the 8 clans in our
                 family today!
