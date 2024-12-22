0d199befe8486b9e0ca046420599caab6b64c177
Ayush Sehrawat <69469790+AyushSehrawat@users.noreply.github.com>
2024-12-22 11:14:46 +0000

    style(config): format code for consistency
    * Updated import statements to use double quotes for consistency.
    * Reformatted JSON structure in `_journal.json` for improved readability.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,13 @@
-import { config } from 'dotenv';
+import { config } from "dotenv";
 import { defineConfig } from "drizzle-kit";
 
-config({ path: '.env' });
+config({ path: ".env" });
 
 export default defineConfig({
-  schema: "./src/lib/server/schema.ts",
-  out: "./src/lib/server/migrations",
-  dialect: "postgresql",
-  dbCredentials: {
-    url: process.env.DATABASE_URL!,
-  },
+    schema: "./src/lib/server/schema.ts",
+    out: "./src/lib/server/migrations",
+    dialect: "postgresql",
+    dbCredentials: {
+        url: process.env.DATABASE_URL!
+    }
 });
