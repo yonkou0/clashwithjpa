91527df8f01125f73e84116d2e5517a8b1046587
Ayush Sehrawat <69469790+AyushSehrawat@users.noreply.github.com>
2024-12-23 17:36:39 +0000

    refactor(data_migrate): improve role checking and rate limit handling
    * Refactored `check_guild_member_role` to return rate limit headers and retry information.
    * Enhanced user role checking in `check_user_exists` with rate limit management.
    * Moved database connection acquisition into individual bulk insert functions to prevent connection release during long-running operations.
    * Cleaned up code for better readability and maintainability.

--- scripts/data_migrate.py
+++ scripts/data_migrate.py
@@ -10,68 +10,114 @@ from aiohttp import ClientSession, ClientConnectionError
 
 DISCORD_BASE = "https://discord.com/api/v10"
 
-async def check_guild_member_role(discord_id: str, session: ClientSession, headers: dict):
+
+async def check_guild_member_role(
+    discord_id: str, session: ClientSession, headers: dict
+):
     guild_id = "1029993902503108678"
     role_id = "1030004174148087878"
-    
+
     try:
-        resp = await session.get(f"{DISCORD_BASE}/guilds/{guild_id}/members/{discord_id}", headers=headers)
+        resp = await session.get(
+            f"{DISCORD_BASE}/guilds/{guild_id}/members/{discord_id}", headers=headers
+        )
+        # X-RateLimit-Limit - The number of requests that can be made
+        # X-RateLimit-Remaining - The number of remaining requests that can be made
+        # X-RateLimit-Reset - Epoch time (seconds since 00:00:00 UTC on January 1, 1970) at which the rate limit resets
+        # X-RateLimit-Reset-After - Total time (in seconds) of when the current rate limit bucket will reset. Can have decimals to match previous millisecond ratelimit precision
+        # X-RateLimit-Bucket - A unique string denoting the rate limit being encountered (non-inclusive of top-level resources in the path)
+        # X-RateLimit-Global - Returned only on HTTP 429 responses if the rate limit encountered is the global rate limit (not per-route)
+        # X-RateLimit-Scope - Returned only on HTTP 429 responses. Value can be user (per bot or user limit), global (per bot or user global limit), or shared (per resource limit)
+
+        x_ratelimit_headers = {
+            "x_ratelimit_limit": resp.headers.get("X-RateLimit-Limit", 0),
+            "x_ratelimit_remaining": resp.headers.get("X-RateLimit-Remaining", 0),
+            "x_ratelimit_reset": resp.headers.get("X-RateLimit-Reset", 0),
+            "x_ratelimit_reset_after": resp.headers.get("X-RateLimit-Reset-After", 0),
+            "x_ratelimit_bucket": resp.headers.get("X-RateLimit-Bucket", 0),
+            "x_ratelimit_global": resp.headers.get("X-RateLimit-Global", 0),
+            "x_ratelimit_scope": resp.headers.get("X-RateLimit-Scope", 0),
+        }
+        retry_after_header = resp.headers.get("Retry-After", 0)
+        print(x_ratelimit_headers, retry_after_header)
+
         if resp.status != 200:
-            return False
-            
+            return (False, x_ratelimit_headers, retry_after_header)
+
         member_data = await resp.json()
-        return str(role_id) in member_data.get("roles", [])
+        return (
+            str(role_id) in member_data.get("roles", []),
+            x_ratelimit_headers,
+            retry_after_header,
+        )
     except ClientConnectionError:
         return False
+
 
 async def check_user_exists(token, cocsData):
     headers = {"Authorization": "Bot " + token}
     result = {}
+    cocsDataLength = len(cocsData)
 
     async with ClientSession() as session:
         for data in cocsData:
             discord_id = data["discordId"]
-            has_role = await check_guild_member_role(discord_id, session, headers)
-            print(f"Discord ID {discord_id}: In server with required role: {has_role}")
+            (
+                has_role,
+                x_ratelimit_headers,
+                retry_after_header,
+            ) = await check_guild_member_role(discord_id, session, headers)
+            print(
+                f"Discord ID {discord_id}: In server with required role: {has_role} | {cocsData.index(data) + 1}/{cocsDataLength}"
+            )
             result[discord_id] = has_role
+
+            remaining_requests = int(x_ratelimit_headers["x_ratelimit_remaining"])
+            if remaining_requests < 2:
+                reset_time_after = float(x_ratelimit_headers["x_ratelimit_reset_after"])
+                print(f"Sleeping for {reset_time_after} seconds")
+                await asyncio.sleep(reset_time_after + 0.1)
 
     return result
 
-async def bulk_bases_insert(conn, basesData):
-    async with conn.transaction():
-        for base in basesData:
-            await conn.execute(
-                """
-                INSERT INTO base_table (code, base_link, image_link)
-                VALUES ($1, $2, $3)
-            """,
-                base["code"],
-                base["baseLink"],
-                base["imageLink"],
-            )
+
+async def bulk_bases_insert(pool, basesData):
+    async with pool.acquire() as conn:
+        async with conn.transaction():
+            for base in basesData:
+                await conn.execute(
+                    """
+                    INSERT INTO base_table (code, base_link, image_link)
+                    VALUES ($1, $2, $3)
+                """,
+                    base["code"],
+                    base["baseLink"],
+                    base["imageLink"],
+                )
 
 
-async def bulk_settings_insert(conn, settingsData):
-    async with conn.transaction():
-        for setting in settingsData:
-            await conn.execute(
-                """
-                INSERT INTO clan_table (clan_code, clan_tag, clan_role_id, member_role_id, elder_role_id, coleader_role_id, leader_role_id, leader_id, channel_id)
-                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
-            """,
-                setting["clanCode"],
-                setting["clanTag"],
-                setting["clanRoleId"],
-                setting["memberRoleId"],
-                setting["elderRoleId"],
-                setting["coleaderRoleId"],
-                setting["leaderRoleId"],
-                setting["leaderId"],
-                setting["channel"],
-            )
+async def bulk_settings_insert(pool, settingsData):
+    async with pool.acquire() as conn:
+        async with conn.transaction():
+            for setting in settingsData:
+                await conn.execute(
+                    """
+                    INSERT INTO clan_table (clan_code, clan_tag, clan_role_id, member_role_id, elder_role_id, coleader_role_id, leader_role_id, leader_id, channel_id)
+                    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+                """,
+                    setting["clanCode"],
+                    setting["clanTag"],
+                    setting["clanRoleId"],
+                    setting["memberRoleId"],
+                    setting["elderRoleId"],
+                    setting["coleaderRoleId"],
+                    setting["leaderRoleId"],
+                    setting["leaderId"],
+                    setting["channel"],
+                )
 
 
-async def bulk_cocs_insert(conn, cocsData):
+async def bulk_cocs_insert(pool, cocsData):
     discord_dat = await check_user_exists(os.getenv("DISCORD_BOT_TOKEN"), cocsData)
 
     userData = set()
@@ -82,23 +128,23 @@ async def bulk_cocs_insert(conn, cocsData):
     for coc in cocsData:
         cocData.append((coc["discordId"], coc["cocTag"]))
 
+    async with pool.acquire() as conn:
+        async with conn.transaction():
+            await conn.executemany(
+                """
+                INSERT INTO user_table (discord_id, is_active)
+                VALUES ($1, $2)
+                """,
+                userData,
+            )
 
-    async with conn.transaction():
-        await conn.executemany(
-            """
-            INSERT INTO user_table (discord_id, is_active)
-            VALUES ($1, $2)
-            """,
-            userData,
-        )
-
-        await conn.executemany(
-            """
-            INSERT INTO coc_table (user_id, tag)
-            VALUES ($1, $2)
-            """,
-            cocData,
-        )
+            await conn.executemany(
+                """
+                INSERT INTO coc_table (user_id, tag)
+                VALUES ($1, $2)
+                """,
+                cocData,
+            )
 
 
 async def main():
@@ -135,20 +181,14 @@ async def main():
 
     print("Data fetched from MongoDB")
 
-    async with pool.acquire() as conn:
-        time = await conn.fetchval("SELECT NOW();")
-        version = await conn.fetchval("SELECT version();")
+    # Connection acquire moved to the functions since long running functions were causing the connection to be released back to the pool
+    await bulk_bases_insert(pool, basesData)
+    await bulk_settings_insert(pool, settingsData)
+    await bulk_cocs_insert(pool, cocsData)
 
-        await bulk_bases_insert(conn, basesData)
-        await bulk_settings_insert(conn, settingsData)
-        await bulk_cocs_insert(conn, cocsData)
-
-        print("Data inserted into PostgreSQL")
+    print("Data inserted into PostgreSQL")
 
     await pool.close()
-
-    print("Current time:", time)
-    print("PostgreSQL version:", version)
 
 
 # Run the asynchronous main function
