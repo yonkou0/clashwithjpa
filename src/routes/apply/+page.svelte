1c0ed8b7613b77970c3bad87e2b04da73cade850
Ayush Sehrawat <69469790+AyushSehrawat@users.noreply.github.com>
2024-12-25 08:30:53 +0000

    feat(apply): add reset functionality to clan application form
    * Implemented `reset` binding to the form for clearing input fields upon updates.
    * Enhanced user experience by ensuring the form resets correctly when updated.

--- src/routes/apply/+page.svelte
+++ src/routes/apply/+page.svelte
@@ -16,7 +16,10 @@
     let { data }: { data: PageData } = $props();
 
     const form = superForm(data.form, {
-        validators: zodClient(clanApplicationSchema)
+        validators: zodClient(clanApplicationSchema),
+        onUpdated() {
+            reset?.();
+        }
     });
     const { form: formData, enhance, message } = form;
     $effect(() => {
@@ -30,6 +33,8 @@
     let buttonDisabled = $derived.by(() => {
         return !$formData.tag || !$formData.apiToken || !$formData["cf-turnstile-response"];
     });
+
+    let reset = $state<() => void>();
 </script>
 
 <main class="flex size-full flex-col items-center justify-center px-5 pb-5 md:px-24 lg:px-32">
@@ -74,7 +79,11 @@
                         on:callback={(event) => {
                             $formData["cf-turnstile-response"] = event.detail.token;
                         }}
+                        on:expired={() => {
+                            reset?.();
+                        }}
                         siteKey={PUBLIC_TURNSTILE_SITE_KEY}
+                        bind:reset
                     />
                 </Field>
                 <button disabled={buttonDisabled} type="submit" class="mt-4 rounded-md bg-white px-4 py-3 text-zinc-800 disabled:bg-zinc-400">
