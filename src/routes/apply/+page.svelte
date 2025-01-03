528f82709b994a0ae68c87080768b382a2ab6c58
swayam25 <178068815+swayam25@users.noreply.github.com>
2025-01-03 08:41:16 +0530

    Update forms

--- src/routes/apply/+page.svelte
+++ src/routes/apply/+page.svelte
@@ -40,7 +40,7 @@
     let reset = $state<() => void>();
 </script>
 
-<main class="flex size-full flex-col items-center justify-center px-5 pb-5 md:px-24 lg:px-32">
+<main class="flex size-full flex-col items-center justify-center">
     {#if data.user && !data.user.inGuild}
         <div class="flex flex-col items-center justify-center">
             <iframe
@@ -56,49 +56,63 @@
             <Button href="https://discord.clashwithjpa.com" class="mt-5" size="md">Join Server</Button>
         </div>
     {:else}
-        <div class="flex items-center justify-center">
-            <form method="POST" action="/apply" use:enhance class="flex w-full max-w-lg flex-col">
-                <Field {form} name="tag">
-                    <Description>Your account tag (include #)</Description>
-                    <Control>
-                        {#snippet children({ props })}
-                            <input {...props} type="text" placeholder="#ABCDEFGHI" bind:value={$formData.tag} class="rounded-md text-zinc-700" />
-                        {/snippet}
-                    </Control>
-                    <FieldErrors class="mt-2 text-red-400" />
-                </Field>
-                <Field {form} name="apiToken">
-                    <Description>Your API token</Description>
-                    <Control>
-                        {#snippet children({ props })}
-                            <input {...props} type="text" placeholder="API Token" bind:value={$formData.apiToken} class="rounded-md text-zinc-700" />
-                        {/snippet}
-                    </Control>
-                    <FieldErrors class="mt-2 text-red-400" />
-                </Field>
-                <Field {form} name="cf-turnstile-response">
-                    <Turnstile
-                        class="mt-2"
-                        on:callback={(event) => {
-                            $formData["cf-turnstile-response"] = event.detail.token;
-                        }}
-                        on:expired={() => {
-                            reset?.();
-                        }}
-                        siteKey={PUBLIC_TURNSTILE_SITE_KEY}
-                        bind:reset
-                    />
-                </Field>
-                <button disabled={buttonDisabled} type="submit" class="mt-4 rounded-md bg-white px-4 py-3 text-zinc-800 disabled:bg-zinc-400">
-                    Submit
-                </button>
-            </form>
-        </div>
-
-        {#if dev}
-            <div class="mt-4 w-full">
-                <SuperDebug data={$formData} />
+        <div class="flex w-screen items-center justify-center lg:justify-start">
+            <!-- Image -->
+            <div
+                class="z-10 hidden h-screen w-1/2 overflow-hidden bg-cover bg-center bg-no-repeat lg:flex items-center justify-center"
+                style="background-image: url('/forms.webp');"
+            >
+                {#if dev}
+                    <div class="w-full p-5">
+                        <SuperDebug data={$formData} />
+                    </div>
+                {/if}
             </div>
-        {/if}
+            <!-- Forms -->
+            <div class="flex items-center justify-center px-5 lg:w-1/2">
+                <form method="POST" action="/apply" use:enhance class="flex w-full max-w-lg flex-col gap-2">
+                    <Field {form} name="tag">
+                        <Description>Your account tag (include #)</Description>
+                        <Control>
+                            {#snippet children({ props })}
+                                <input {...props} type="text" placeholder="#ABCDEFGHI" bind:value={$formData.tag} class="rounded-xl text-gray-700" />
+                            {/snippet}
+                        </Control>
+                        <FieldErrors class="text-red-400" />
+                    </Field>
+                    <Field {form} name="apiToken">
+                        <Description>Your API token</Description>
+                        <Control>
+                            {#snippet children({ props })}
+                                <input
+                                    {...props}
+                                    type="text"
+                                    placeholder="API Token"
+                                    bind:value={$formData.apiToken}
+                                    class="rounded-xl text-gray-700"
+                                />
+                            {/snippet}
+                        </Control>
+                        <FieldErrors class="text-red-400" />
+                    </Field>
+                    <Field {form} name="cf-turnstile-response">
+                        <Turnstile
+                            class="text-center"
+                            on:callback={(event) => {
+                                $formData["cf-turnstile-response"] = event.detail.token;
+                            }}
+                            on:expired={() => {
+                                reset?.();
+                            }}
+                            siteKey={PUBLIC_TURNSTILE_SITE_KEY}
+                            bind:reset
+                        />
+                    </Field>
+                    <button disabled={buttonDisabled} type="submit" class="mt-4 rounded-xl bg-white px-4 py-3 text-gray-800 disabled:bg-gray-400">
+                        Submit
+                    </button>
+                </form>
+            </div>
+        </div>
     {/if}
 </main>
