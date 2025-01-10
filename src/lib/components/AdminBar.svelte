8bccdbe4b315d08d591cd61f669fc6a6c1b53357
swayam25 <178068815+swayam25@users.noreply.github.com>
2025-01-10 11:20:52 +0530

    Refactor navbars color

--- src/lib/components/AdminBar.svelte
+++ src/lib/components/AdminBar.svelte
@@ -36,12 +36,12 @@
 </script>
 
 <nav
-    class="max-w-screen sticky inset-x-0 bottom-0 z-20 flex items-center justify-evenly gap-2 rounded-t-xl bg-gray-800 p-2 md:py-4 backdrop-blur-sm transition-all md:inset-y-0 md:left-0 md:mt-[80px] md:max-h-screen md:max-w-lg md:flex-col md:justify-start md:rounded-t-none md:rounded-br-xl"
+    class="max-w-screen sticky inset-x-0 bottom-0 z-20 flex items-center justify-evenly gap-2 rounded-t-xl bg-gray-900 p-2 md:py-4 backdrop-blur-sm transition-all md:inset-y-0 md:left-0 md:mt-[80px] md:max-h-screen md:max-w-lg md:flex-col md:justify-start md:rounded-t-none md:rounded-br-xl"
 >
     {#each items as item}
         <a
             href={item.href}
-            class="flex w-full flex-col items-center justify-start rounded-xl px-5 py-2 transition-all hover:bg-gray-900/50 md:flex-row md:gap-2"
+            class="flex w-full flex-col items-center justify-start rounded-xl px-5 py-2 transition-all duration-200 hover:bg-gray-950/50 md:flex-row md:gap-2"
         >
             <item.icon class="size-6 md:size-8" />
             <span class="text-[8px] md:text-base">{item.name}</span>
