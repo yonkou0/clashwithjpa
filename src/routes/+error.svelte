ae802504caecd2d6d9517994d5fd2665fd2ce445
swayam25 <178068815+swayam25@users.noreply.github.com>
2025-02-06 16:49:40 +0530

    Update error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import { page } from "$app/state";
     import CocButton from "$lib/components/CocButton.svelte";
+    import H1 from "$lib/components/H1.svelte";
     import MaterialSymbolsChevronLeftRounded from "~icons/material-symbols/chevron-left-rounded";
 </script>
 
@@ -9,12 +10,12 @@
 </svelte:head>
 
 <div class="size-full bg-cover">
-    <img src="/bg_error.webp" alt="Error" class="fixed top-0 left-0 -z-10 size-full object-cover" />
+    <img src="/bg_error.webp" alt="Error" class="fixed inset-0 -z-10 size-full object-cover" />
 
     <div class="flex size-full items-center justify-center overflow-hidden bg-gray-950/50">
         <div class="flex size-full flex-col items-center justify-center space-y-1 p-5">
-            <h1 class="text-4xl font-bold md:text-6xl lg:text-5xl">{page.status}</h1>
-            <h2 class="text-xl font-semibold text-gray-400 lg:text-2xl">{page.error?.message}</h2>
+            <H1 class="text-4xl font-bold md:text-6xl">{page.status}</H1>
+            <h2 class="text-xl font-semibold text-gray-300 lg:text-2xl">{page.error?.message}</h2>
             <div class="pt-5">
                 <CocButton href="/" size="md" class="group">
                     <MaterialSymbolsChevronLeftRounded class="size-8 transition-transform group-hover:-translate-x-2" />
