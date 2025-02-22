f71781508c3c220599a8497de5a3ccf6898f7323
swayam25 <178068815+swayam25@users.noreply.github.com>
2025-02-22 15:21:09 +0530

    Minor fixes

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -32,7 +32,7 @@
     class="fixed top-0 z-100 flex max-h-screen w-full flex-col items-center p-4 px-6 backdrop-blur-xs transition-all md:px-12 lg:px-28"
     class:rounded-b-2xl={!isOpen}
     class:md:rounded-bl-none={page.url.href.includes("/admin")}
-    class:bg-gray-900={page.url.href.includes("/admin")}
+    class:bg-gray-900={page.url.href.includes("/admin") && page.error?.message !== "Not Found"}
     class:!backdrop-blur-md={isOpen}
 >
     <div class="flex w-full items-center justify-between">
