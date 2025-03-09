b446f47bed8e9ca2d78d98d95c954f93c65ee12d
swayam25 <178068815+swayam25@users.noreply.github.com>
2025-03-09 22:31:02 +0530

    Fix `z-index` & Update background image

--- src/lib/components/UserButton.svelte
+++ src/lib/components/UserButton.svelte
@@ -32,7 +32,7 @@
             <img src="https://media.discordapp.net/avatars/{user.id}/{user.avatar}" alt="Avatar" class="size-8 rounded-full lg:size-11" />
         </Popover.Trigger>
         <Popover.Portal>
-            <Popover.Content class="z-20 max-w-80 rounded-xl p-2">
+            <Popover.Content class="z-200 max-w-80 rounded-xl p-2">
                 <div
                     class="flex flex-col gap-5 rounded-xl border border-gray-950 bg-linear-to-b from-gray-800 via-gray-800 to-gray-900 p-5 shadow-[0_0_5px_0.5px_var(--tw-shadow-color)] shadow-gray-950 backdrop-blur-md"
                 >
